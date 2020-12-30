2d9cff04849bbb9c71981cff741b5da6620218fa
Danil <danya.dyakonov@mail.ru>
2020-12-30 18:27:36 +0300

    Adding project with gitignore

---
+++ WebApplication3/Models/AppUser.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using Microsoft.AspNetCore.Identity;
+using NUnit.Framework;
+
+namespace WebApplication3.Models
+{
+    public class User : IdentityUser
+    {
+        public override string Id { get; set; }
+        public override string Email { get; set; }
+
+        [DefaultValue("'{}'::text[]")]
+        public List<string> FavoriteTracks { get; set; }
+    }
+}
---
+++ WebApplication3/Views/Account/Profile.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication3.Views.Account
+{
+    public class Profile : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
